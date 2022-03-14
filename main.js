1b5a57b2bcc79e6d7ffe2158be271d4d270ca77e
blablass <82516957+blablass@users.noreply.github.com>
2022-03-14 18:55:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-//https://teachablemachine.withgoogle.com/models/LVLN_85lX/
+
+function properfunction(){
+    navigator.mediaDevices.getUserMedia({
+        audio:true
+    });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/LVLN_85lX/model.json",modelready)
+}
+function modelready(){
+    classifier.classify(gotresult)
+}
+function gotresult(error,result){
+    if(error){
+        console.log(error)
+    }else{
+        console.log(result)
+    }
+}
