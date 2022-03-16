f18a82ce0d8f50563a02f654cfcea04ea642e865
blablass <82516957+blablass@users.noreply.github.com>
2022-03-16 18:35:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -13,5 +13,39 @@ function gotresult(error,result){
         console.log(error)
     }else{
         console.log(result)
+        random_r = Math.floor(Math.random()*255) + 1
+        random_g = Math.floor(Math.random()*255) + 1
+        random_b = Math.floor(Math.random()*255) + 1
+        document.getElementById("resultlabel").innerHTML = "I can hear: " + result[0].label
+        document.getElementById("confidence").innerHTML = "Accuracy: " + (result[0].confidence * 100).toFixed(3) + "%"
+        document.getElementById("resultlabel").style.color = "rgb("+ random_r +","+ random_g +","+ random_b +")"
+        document.getElementById("confidence").style.color = "rgb("+ random_r +","+ random_g +","+ random_b +")"
+        img1 = document.getElementById("alien1");
+        img2 = document.getElementById("alien2");
+        img3 = document.getElementById("alien3");
+        img4 = document.getElementById("alien4");
+        if(result[0].label == "clap"){
+            img1.src = "aliens-01.gif"
+            img2.src = "aliens-02.png"
+            img3.src = "aliens-03.png"
+            img4.src = "aliens-04.png"
+        }
+        else if(result[0].label == "snap"){
+            img1.src = "aliens-01.png"
+            img2.src = "aliens-02.gif"
+            img3.src = "aliens-03.png"
+            img4.src = "aliens-04.png"
+        }
+        else if(result[0].label == "Bell"){
+            img1.src = "aliens-01.png"
+            img2.src = "aliens-02.png"
+            img3.src = "aliens-03.gif"
+            img4.src = "aliens-04.png"
+        } 
+        else{           img1.src = "aliens-01.png"
+            img2.src = "aliens-02.png"
+            img3.src = "aliens-03.png"
+            img4.src = "aliens-04.gif"
+        }
     }
 }
